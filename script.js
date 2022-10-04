ed6ad2f9b3b7b8372b09e8f6e97d373dbc8f3279
Wellington_Fonseca <thyrso.kof@gmail.com>
2022-10-04 19:44:35 -0300

    Enviando para o github

--- script.js
+++ script.js
@@ -4,58 +4,53 @@ const h2 = document.getElementById('meme-text');
 const selectedImage = document.getElementById('meme-image');
 const chooseImage = document.getElementById('meme-insert');
 const preMemes = document.querySelector('#imgs').children;
-const btn = document.querySelectorAll("button");
+const btn = document.querySelectorAll('button');
 
-function showMemeText(){
-h2.innerText = input.value;
+function showMemeText() {
+  h2.innerText = input.value;
 }
 
 input.addEventListener('keyup', showMemeText);
 
-function insertImage(event){
-   const selectedFile = event.target.files[0];
-console.log(selectedFile);
-   const addressFile = new FileReader();
-   addressFile.onloadend = function(){
-       selectedImage.setAttribute('src', addressFile.result);
-   }
-   addressFile.readAsDataURL(selectedFile);
-  
+function insertImage(event) {
+  const selectedFile = event.target.files[0];
+  console.log(selectedFile);
+  const addressFile = new FileReader();
+  addressFile.onloadend = function addSrcValue() {
+    selectedImage.setAttribute('src', addressFile.result);
+  };
+  addressFile.readAsDataURL(selectedFile);
 }
-    
+
 chooseImage.addEventListener('change', insertImage);
 
-
-function changeBorderContainer(event){
-    if(event.target.id === 'fire'){
-        memePlace.style.border = '3px dashed red';
-    }else if(event.target.id === 'water'){
-        memePlace.style.border = '5px double blue';
-    }else if(event.target.id === 'earth'){
-        memePlace.style.border = '6px groove green';
-    }
+function changeBorderContainer(event) {
+  if (event.target.id === 'fire') {
+    memePlace.style.border = '3px dashed red';
+  } else if (event.target.id === 'water') {
+    memePlace.style.border = '5px double blue';
+  } else if (event.target.id === 'earth') {
+    memePlace.style.border = '6px groove green';
+  }
 }
 
-function addEventOnButtons(){
-    for(let key of btn){
-        key.addEventListener('click', changeBorderContainer);
-    }
-
+function addEventOnButtons() {
+  // eslint-disable-next-line no-restricted-syntax
+  for (const key of btn) {
+    key.addEventListener('click', changeBorderContainer);
+  }
 }
 addEventOnButtons();
 
-
-function changeMemeWithPreMemes(event){
-    selectedImage.src = event.target.src;
+function changeMemeWithPreMemes(event) {
+  selectedImage.src = event.target.src;
 }
 
-function addEventInMemes(){
-    for(let key of preMemes){
-        key.addEventListener('click', changeMemeWithPreMemes);
-
-    }
-
+function addEventInMemes() {
+  // eslint-disable-next-line no-restricted-syntax
+  for (const key of preMemes) {
+    key.addEventListener('click', changeMemeWithPreMemes);
+  }
 }
 
 addEventInMemes();
-
